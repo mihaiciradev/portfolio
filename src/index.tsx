8227c3200c26dfcb5ea891425d7e07e91652f9c7
mihaiciradev <mihai.cira.dev@gmail.com>
2023-05-02 11:40:03 +0300

    added analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,28 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4uLkXdsarc6q-aaXhMyBGsP4qeo0wPzA",
+  authDomain: "portfolio-3d619.firebaseapp.com",
+  projectId: "portfolio-3d619",
+  storageBucket: "portfolio-3d619.appspot.com",
+  messagingSenderId: "71711927007",
+  appId: "1:71711927007:web:3daa1c20c72bdf0492576a",
+  measurementId: "G-QYY5W8KSBH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
